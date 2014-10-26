3178e330d22b83e9b54d1ae743bf2673660565aa
Jia-Shen Boon <jiashen@gmail.com>
2014-10-26 01:01:07 -0500

    Test classifier - got 30% accuracy. Win.

--- naive_bayes_classifier.m
+++ naive_bayes_classifier.m
@@ -1,22 +1,36 @@
 % Train a Naive Bayes Classifier to classifiy speculative language
 
-%% Get unique set of words
+%% Pre-process sentences
+% Remove commas, periods, brackets () from all sentences
+% Remove trailing spaces (otherwise strsplit will return
+
+fprintf('Pre-processing sentences... '); tic;
+for si = 1: size(labeled_sentences, 1)
+    sentence = labeled_sentences{si, 1};
+    sentence = regexprep(sentence, '[,\.\(\)]', '');
+    sentence = strtrim(sentence);
+    labeled_sentences{si, 1} = sentence;
+end
+fprintf('done.\n'); toc;
+
+%% Partition dataset to training and test set
 
 % Shuffle sentences
 n_labeled_sentences = size(labeled_sentences, 1);
 labeled_sentences = labeled_sentences(randperm(n_labeled_sentences), :);
 
-% Get training set
-train_size = floor(size(labeled_sentences, 1) / 2);  % Size of training set
+% Partition out training set from dataset
+train_size = floor(0.5 * size(labeled_sentences, 1));  % Size of training set
 Xtrain = labeled_sentences(1:train_size, 1);
 ytrain = cell2mat(labeled_sentences(1:train_size, 2));
 
-% Remove commas, periods, brackets () from sentences in training set
-% Remove trailing spaces (otherwise strsplit will return
-for si = 1: length(Xtrain)
-    Xtrain{si} = regexprep(Xtrain{si}, '[,\.\(\)]', '');
-    Xtrain{si} = strtrim(Xtrain{si});
-end
+% Partition out test set from dataset
+test_size = floor(0.3 * size(labeled_sentences, 1));  % Size of test set
+test_size = min(500, test_size);
+Xtest = labeled_sentences(train_size+1:train_size+test_size, 1);
+ytest = cell2mat(labeled_sentences(train_size+1:train_size+test_size, 2));
+
+%% Get unique set of words
 
 % Get unique words (we call this the dictionary)
 sentences_concatenated = strjoin(Xtrain');  % Adds a space delimiter
@@ -39,6 +53,7 @@ fprintf('No. of sentences in training set: %i\n', length(Xtrain));
 
 %% Populate marginal probabilities with a hash table
 
+fprintf('Calculating marginal probabilities of words... '); tic;
 for si = 1:length(Xtrain)
 
     % Get unique words in this sentence
@@ -54,24 +69,55 @@ for si = 1:length(Xtrain)
         end
     end
     
-    if mod(si, 1000) == 1
-        fprintf('%5i ', si);
-    end
 end
 
 % Marginal probabilities, P(word | speculative) & P(word | unspeculative)
-spec_marginals = containers.Map(dictionary, cell_of_zeros);
-unspec_marginals = containers.Map(dictionary, cell_of_zeros);
+spec_logmarginals = containers.Map(dictionary, cell_of_zeros);
+unspec_logmarginals = containers.Map(dictionary, cell_of_zeros);
 
 % Normalize marginal probabilities
+% We take the log10 of these probabilities so they don't go too close to
+% zero.
+% We also perform Laplacian smoothing by hallucinating 1 example from each
+% class.
 n_speculative = sum(ytrain);
 n_unspeculative = length(Xtrain) - n_speculative;
 for wi = 1:length(dictionary)
     word = dictionary{wi};
     
     % P(word | spec) = #(spec sentences with word) / #(spec sentences)
-    spec_marginals(word) = spec_counts(word) / n_speculative;
+    spec_logmarginals(word) = log10((spec_counts(word)+1) / (n_speculative+2));
     
     % P(word | unspec) = #(unspec sentences with word) / #(unspec sentences)
-    unspec_marginals(word) = unspec_counts(word) / n_unspeculative;
+    unspec_logmarginals(word) = log10((unspec_counts(word)+1) / (n_unspeculative+2));
 end
+
+fprintf('done.\n'); toc;
+
+%% Test classifier
+
+fprintf('Testing %i examples... ', test_size); tic;
+predictions = zeros(test_size, 1);
+for si = 1:test_size
+    
+    % Get unique words in this sentence
+    sentence = Xtest{si};
+    words = unique(strsplit(sentence));
+    
+    % Only look for words that are in the dictionary
+    words_in_dictionary = words(ismember(words, dictionary));
+    
+    % Get non-normalize posterior probability for speculative language
+    nonnorm_spec = sum(cell2mat(spec_logmarginals.values(words_in_dictionary)));
+    
+    % Get non-normalize posterior probability for unspeculative language
+    nonnorm_unspec = sum(cell2mat(unspec_logmarginals.values(words_in_dictionary)));
+    
+    % The predicted class is the argmax of the non-normalized probabilities
+    % over the possible classes (i.e. speculative or non-speculative)
+    prediction = nonnorm_spec > nonnorm_unspec;
+    
+    predictions(si) = prediction;
+
+end
+fprintf('done.\n'); toc;
