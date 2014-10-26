247e3cbb2a204ab76a7ac801c61ab3d6c375320e
Jia-Shen Boon <jiashen@gmail.com>
2014-10-26 00:08:27 -0500

    Get marginal probabilities with hash table
    Previously this was by regular MATLAB matrices. Hash tables speed calculations
    up significantly since lookup is constant time.

--- naive_bayes_classifier.m
+++ naive_bayes_classifier.m
@@ -2,19 +2,24 @@
 
 %% Get unique set of words
 
-% Get sentences and labels in their own variables
-all_sentences = labeled_sentences(:, 1);
-are_speculative = cell2mat(labeled_sentences(:, 2));
+% Shuffle sentences
+n_labeled_sentences = size(labeled_sentences, 1);
+labeled_sentences = labeled_sentences(randperm(n_labeled_sentences), :);
 
-% Remove commas, periods, brackets () from sentences
+% Get training set
+train_size = floor(size(labeled_sentences, 1) / 2);  % Size of training set
+Xtrain = labeled_sentences(1:train_size, 1);
+ytrain = cell2mat(labeled_sentences(1:train_size, 2));
+
+% Remove commas, periods, brackets () from sentences in training set
 % Remove trailing spaces (otherwise strsplit will return
-for si = 1: length(all_sentences)
-    all_sentences{si} = regexprep(all_sentences{si}, '[,\.\(\)]', '');
-    all_sentences{si} = strtrim(all_sentences{si});
+for si = 1: length(Xtrain)
+    Xtrain{si} = regexprep(Xtrain{si}, '[,\.\(\)]', '');
+    Xtrain{si} = strtrim(Xtrain{si});
 end
 
 % Get unique words (we call this the dictionary)
-sentences_concatenated = strjoin(all_sentences');  % Adds a space delimiter
+sentences_concatenated = strjoin(Xtrain');  % Adds a space delimiter
 all_words = strsplit(sentences_concatenated);
 dictionary = unique(all_words);
 
@@ -22,31 +27,30 @@ dictionary = unique(all_words);
 
 n_unique_words = length(dictionary);
 
-% No. of sentences that a word appears in
-sentence_counts = zeros(n_unique_words, 1);
-
 % No. of speculative sentences that a word appears in
-spec_counts = zeros(n_unique_words, 1);
+cell_of_zeros = num2cell(zeros(1, n_unique_words));
+spec_counts = containers.Map(dictionary, cell_of_zeros);
+
+% No. of unspeculative sentences that a word appears in
+unspec_counts = containers.Map(dictionary, cell_of_zeros);
 
 fprintf('No. of unique words: %i\n', n_unique_words);
-fprintf('No. of sentences: %i\n', length(all_sentences));
+fprintf('No. of sentences in training set: %i\n', length(Xtrain));
 
 %% Populate marginal probabilities with a hash table
-sentence_counts = containers.Map(dictionary, num2cell(zeros(1, n_unique_words)));
-spec_counts = containers.Map(dictionary, num2cell(zeros(1, n_unique_words)));
 
-for si = 1:length(all_sentences)
+for si = 1:length(Xtrain)
 
     % Get unique words in this sentence
-    sentence = all_sentences{si};
+    sentence = Xtrain{si};
     words = unique(strsplit(sentence));
 
     for wi = 1:length(words)
         word = words{wi};
-        sentence_counts(word) = sentence_counts(word) + 1;
-
-        if are_speculative(si)
+        if ytrain(si)
             spec_counts(word) = spec_counts(word) + 1;
+        else
+            unspec_counts(word) = unspec_counts(word) + 1;
         end
     end
     
@@ -54,51 +58,20 @@ for si = 1:length(all_sentences)
         fprintf('%5i ', si);
     end
 end
-%% Populate marginal probablilities by iterating over sentences
 
-if false
-    for si = 1:length(all_sentences)
+% Marginal probabilities, P(word | speculative) & P(word | unspeculative)
+spec_marginals = containers.Map(dictionary, cell_of_zeros);
+unspec_marginals = containers.Map(dictionary, cell_of_zeros);
 
-        % Get unique words in this sentence
-        sentence = all_sentences{si};
-        words = unique(strsplit(sentence));
-
-        % Get the index of these words in the dictionary
-        mask = ismember(dictionary, words);
-
-        % Update marginal probabilities
-        sentence_counts(mask) = sentence_counts(mask) + 1;
-        if are_speculative(si)
-            spec_counts(mask) = spec_counts(mask) + 1;
-        end
-
-        if mod(si, 100) == 1
-            fprintf('%5i ', si);
-        end
-    end
-end
-
-%% Populating marginal probabilities by iterating over dictionary
-
-if false
-    for wi = 1: n_unique_words
-
-        word = unique_words{wi};
-        occurrence_inds = strfind(labeled_sentences(:, 1), word);
-        does_appear = ~cellfun(@isempty, occurrence_inds);
-
-        % No. of sentences that a word appears in
-        sentence_count = sum(does_appear);
-
-        % No. of speculative sentences that a word appears in
-        spec_count = sum(does_appear & are_speculative);
-
-        % Update marginal probabilities
-        sentence_counts(wi) = sentence_count;
-        spec_counts(wi) = spec_count;
-
-        if mod(wi, 100) == 1
-            fprintf('%i ', wi);
-        end
-    end
+% Normalize marginal probabilities
+n_speculative = sum(ytrain);
+n_unspeculative = length(Xtrain) - n_speculative;
+for wi = 1:length(dictionary)
+    word = dictionary{wi};
+    
+    % P(word | spec) = #(spec sentences with word) / #(spec sentences)
+    spec_marginals(word) = spec_counts(word) / n_speculative;
+    
+    % P(word | unspec) = #(unspec sentences with word) / #(unspec sentences)
+    unspec_marginals(word) = unspec_counts(word) / n_unspeculative;
 end
