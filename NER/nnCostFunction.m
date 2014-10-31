804b1944d4ba22c198ea7a60b175ae391053a897
Jia-Shen Boon <jiashen@gmail.com>
2014-10-31 12:38:36 -0500

    Constrain ANN cost to network of 1 output unit
    Previously the network could be have multiple units in the output layer. This
    simplifies the code, and is still applicable to the NER assignment which only
    has one output unit.

--- NER/nnCostFunction.m
+++ NER/nnCostFunction.m
@@ -2,18 +2,20 @@ function [J grad] = nnCostFunction(nn_params, ...
                                    input_layer_size, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
-                                   X, y, lambda)
+                                   x, y, lambda)
 %NNCOSTFUNCTION Implements the neural network cost function for a two layer
-%neural network which performs classification
+%neural network which performs classification, for a single training
+%example
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. X is
-%   of size (no. of examples)x(size of one example). y is of size (no. of
-%   examples)x(size of output layer)
+%   nn_params and need to be converted back into the weight matrices.
+%   x is a input_layer_size-length column vector
+%   y is a scalar.
 % 
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
+%   The sequence of parameters is [b1(:) W1(:) b2(:) W2(:) x]
 %
 
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
@@ -24,9 +26,8 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
 
-% Setup some useful variables
-m = size(X, 1);  % number of examples
-         
+assert(num_labels == 1);  % right now only works with 1 output unit
+
 % You need to return the following variables correctly 
 J = 0;
 Theta1_grad = zeros(size(Theta1));
@@ -64,36 +65,32 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 
 % Notation: s_l is the no. of units (excluding bias) in the l-layer
-% n is the no. of training examples
-a1 = X'; % activation of input layer - size s_1 x n
-z2 = Theta1*[ones(1,m);a1]; % input to hidden layer - size s_2 x n
-a2 = sigmoid(z2); % activation of hidden layer - size s_2 x n
-z3 = Theta2*[ones(1,m) ; a2]; % input to output layer - size s_3 x n
-a3 = sigmoid(z3); % activation of output layer - size s_3 x n
+a1 = x'; % activation of input layer - size s_1 x 1
+z2 = Theta1 * [1; a1]; % input to hidden layer - size s_2 x 1
+a2 = sigmoid(z2); % activation of hidden layer - size s_2 x 1
+z3 = Theta2 * [1; a2]; % input to output layer - size s_3 x 1
+a3 = sigmoid(z3); % activation of output layer - size s_3 x 1
 
 % This converts y to 1-hot encoding
-y_1hot = (repmat(y',[num_labels 1]) == repmat((1:num_labels)',[1 m]) );
+y_1hot = y;
 
 % Get binary cross entropy cost (non-regularized)
 J = y_1hot .* -log(a3) + (1-y_1hot) .* -(log(1-a3));
-J = sum(J(:)) / m;
 
 % Get cost (regularized)
 sumTheta1 = sum(sum(Theta1(:,2:end).^2));
 sumTheta2 = sum(sum(Theta2(:,2:end).^2));
-J = J + lambda/(2*m)*(sumTheta1+sumTheta2);
+J = J + lambda / 2 * (sumTheta1 + sumTheta2);
 
 % Get gradients by interating over examples
-assert(m == 1)
-ei = 1;  % TODO make this work with multiple training examples
-delta3 = a3(:,ei) - y_1hot(:,ei); % size of 10x1
-Theta2_grad = Theta2_grad + delta3*[1;a2(:,ei)]'; % size of Theta2 (10x26)
-delta2 = (Theta2(:,2:end)'*delta3).*sigmoidGradient(z2(:,ei)); % size of 25x1
-Theta1_grad = Theta1_grad + delta2*[1;a1(:,ei)]'; % size of Theta1 (25x401)
+delta3 = a3 - y_1hot;
+Theta2_grad = delta3 * [1; a2]'; % size of Theta2
+delta2 = (Theta2(:,2:end)'*delta3) .* sigmoidGradient(z2);
+Theta1_grad = delta2 * [1; a1]'; % size of Theta1
 
 % Add the regularization term
-Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + lambda/m*Theta1(:,2:end);
-Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + lambda/m*Theta2(:,2:end);
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + lambda*Theta1(:,2:end);
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + lambda*Theta2(:,2:end);
 
 
 
