ebcec683d7ae7e4a239433fba9e871590397379b
Jia-Shen Boon <jiashen@gmail.com>
2014-11-13 00:16:17 -0600

    Train word vectors from all BioScope data

--- word2vec/BioScope_word2vec.py
+++ word2vec/BioScope_word2vec.py
@@ -8,17 +8,30 @@ import nltk
 from bs4 import BeautifulSoup
 
 
-def get_sentences_from_Bioscope(filename):
-    "Return a list of sentences given a BioScope XML filename."
+class get_tokenized_sentences_from_Bioscope(object):
+    """Generate tokenized sentences given BioScope XML filenames.
+    
+    >>> for sentence in get_sentences_from_Bioscope(['abstracts.xml']):
+    ...     print sentence
+    """
 
-    # Load a list of sentences
-    fid = open(filename)
-    textblock = fid.read()
-    fid.close()
-    soup = BeautifulSoup(textblock)
-    sentences = [tag.get_text() for tag in soup.find_all('sentence')]
+    def __init__(self, filenames):
+        self.filenames = filenames
 
-    return sentences
+    def __iter__(self):
+
+        for filename in self.filenames:
+
+            # Load a list of sentences
+            fid = open(filename)
+            textblock = fid.read()
+            fid.close()
+            soup = BeautifulSoup(textblock)
+            sentences = [tag.get_text() for tag in soup.find_all('sentence')]
+
+            # Yield sentence by sentence
+            for sentence in sentences:
+                yield [word.lower() for word in nltk.word_tokenize(sentence)]
 
 if __name__ == "__main__":
 
@@ -29,20 +42,14 @@ if __name__ == "__main__":
     filenames = ["../data/abstracts.xml", "../data/abstracts_pmid.xml",
             "../data/full_papers.xml"]
 
-    # Get sentences from BioScope corpus
-    sentences = get_sentences_from_Bioscope('../data/abstracts.xml')
-
-
-
-    # Tokenize sentences into words and make lower case
-    tokenized_sentences = [
-            [word.lower() for word in nltk.word_tokenize(sentence)]
-            for sentence in sentences]
+    # Get tokenized sentences from BioScope corpus
+    tokenized_sentences = get_tokenized_sentences_from_Bioscope(filenames)
 
     get_sentences = lambda: tokenized_sentences;
 
     if verbose:
-        print 'Training', len(tokenized_sentences), 'sentences'
+        print 'Training model with %i sentences...' %  \
+                len([i for i in get_sentences()])
 
     # Train word2vec model
     model = gensim.models.Word2Vec(
@@ -50,28 +57,3 @@ if __name__ == "__main__":
             min_count=5)
     model.build_vocab(get_sentences())
     model.train(get_sentences())
-
-    assert False
-
-    # Word matrix, each row is the vector of a word
-    vocab = list(model.vocab.keys())
-    word_matrix = np.array([model[word] for word in vocab])
-
-    # K-means cluster all word vectors
-    n_clusters = 10
-    _, clusters = scipy.cluster.vq.kmeans2(word_matrix, n_clusters)
-
-    # Print size of each cluster
-    for ci in range(n_clusters):
-        print('%i words in cluster %i' % (sum(clusters == ci), ci))
-
-    # Print some words from each cluster
-    print('\nRandom words in each cluster')
-    n_words = 10
-    for ci in range(n_clusters):
-
-        # Get indices of words in word matrix V
-        vis = np.flatnonzero(clusters == ci)[:n_words]
-        words = (vocab[vi] for vi in vis)
-
-        print('Cluster %i: %s' % (ci, ' '.join(words)))
