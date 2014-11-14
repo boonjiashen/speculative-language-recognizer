ea4331a19e809bfa80bd4876c1bc23db0fb40144
Jia-Shen Boon <jiashen@gmail.com>
2014-11-14 14:15:08 -0600

    Turn hard-coded arguments to cmd line arguments
    Previously, arguments in the script like verbosity and training data files were
    hardcoded into the script. We now make the user supply these arguments through
    the command line, for flexibility.

--- word2vec/BioScope_word2vec.py
+++ word2vec/BioScope_word2vec.py
@@ -5,6 +5,7 @@ import gensim
 import numpy as np
 import itertools
 import nltk
+import argparse
 from bs4 import BeautifulSoup
 
 
@@ -35,20 +36,39 @@ class get_tokenized_sentences_from_Bioscope(object):
             for sentence in sentences:
                 yield [word.lower() for word in nltk.word_tokenize(sentence)]
 
+
 if __name__ == "__main__":
 
-    verbose = True  # True if you want to print stages that the script is at during
-                    # run time
+
+    ######################## Parse command-line arguments ##################### 
+
+    parser = argparse.ArgumentParser()
+
+    # Add argument for more verbose stdout
+    parser.add_argument("-v", "--verbose",
+            help="print status during program execution", action="store_true")
+
+    # Add required argument of training data
+    parser.add_argument('filenames', metavar='filepath', type=str, nargs='+',
+                               help='one or more XML files used as training data')
+
+    # Grab arguments from stdin
+    args = parser.parse_args()
 
     # Filenames of BioScope XML files
     filenames = ["../data/abstracts.xml", "../data/full_papers.xml"]
+    filenames = ["../data/one_abstract.xml"]
+    filenames = args.filenames
+
+
+    ######################### Train model ##################################### 
 
     # Get tokenized sentences from BioScope corpus
     tokenized_sentences = get_tokenized_sentences_from_Bioscope(filenames)
 
     get_sentences = lambda: tokenized_sentences;
 
-    if verbose:
+    if args.verbose:
         print 'Training model with %i sentences...' %  \
                 len([i for i in get_sentences()])
 
