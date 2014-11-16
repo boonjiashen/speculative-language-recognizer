9eb50b8571abd51bc8ce8409113f2577b39bcc44
Akshay Sood <sood@cs.wisc.edu>
2014-11-16 15:59:28 -0600

    Create new Eclipse project for speculative language recognition using RNTN

---
+++ rntn/src/speculative/Test.java
@@ -0,0 +1,12 @@
+package speculative;
+
+import edu.stanford.nlp.sentiment.RNNOptions;
+
+public class Test {
+
+    public static void main(String[] args) {
+        for (String s : RNNOptions.DEFAULT_CLASS_NAMES) {
+            System.out.println(s);
+        }
+    }
+}
