8fbc0cf5e1b1a11fcf354aca9d945ffe9644e19e
Jia-Shen Boon <jiashen@gmail.com>
2014-11-27 23:18:11 -0600

    Pull Word2VecScorer to its own file

--- word2vec/BioScope_word2vec.py
+++ word2vec/BioScope_word2vec.py
@@ -7,90 +7,7 @@ import itertools
 import nltk
 import argparse
 from bs4 import BeautifulSoup
-
-class Word2VecScorer(object):
-
-
-    def __init__(self, model):
-        "Takes in a word2vec or doc2vec model"
-        self.comparisons = []  # a list of 4-ples
-        self._build_comparisons(model)
-
-    def _build_comparisons(self, model):
-        """Build the list of comparisons appropriate for this model.
-
-        We only make use of grammar comparisons since it's the only categories
-        where our training data has the required vocabulary. We also ignore
-        comparisons if any of the four items in the comparison are not in the
-        model's vocabulary.
-        """
-
-        # Each line is a comparison 'x y X Y' i.e. x is to y what X is to Y EXCEPT
-        # for categories, which are lines that start with a colon, e.g.
-        #: city-in-state
-        #: family
-        #: gram1-adjective-to-adverb
-        #: gram2-opposite
-        evaluation_filename = 'data/questions-words.txt'
-        with open(evaluation_filename) as fid:
-            lines = fid.read().splitlines()
-
-        # Find out where the grammar categories start.
-        start_ind = [ind for ind, line in enumerate(lines)
-                if line[0] == ':' and 'gram' in line][0]
-
-        # Grab all grammar comparisons as a list of 4-ples
-        # We happen to know all the grammar comparisons are at the end of the file.
-        vocab = set(model.vocab.keys())
-        for line in lines:
-
-            # Skip category headers
-            if line[0] == ':':
-                continue
-
-            # Don't bother testing if any of these words aren't in vocab
-            tokens = line.split()
-            if not set(tokens) <= vocab:
-                continue
-
-            self.comparisons.append(tokens)
-
-    def score(self, model, topn, return_correct_comparisons=False):
-        """Score a model by how many correct comparisons it makes from the
-        set of analogies
-
-        topn - correct answer should be within top #N predictions
-        """
-
-        # Start comparisons
-        correct_inds = []  # Inds of comparisons that were predicted right
-        for ci, comparison in enumerate(self.comparisons):
-
-            # E.g. 'stupid clever slow fast'
-            # which implies stupid is to clever what slow is to fast.
-            # We want to predict 'fast' from the first three words
-            pos1, neg1, pos2, neg2 = comparison
-
-            try:
-
-                # Make predictions (these come with confidence levels)
-                predictions_and_confidence = model.most_similar(
-                        positive=[pos1, pos2], negative=[neg1], topn=topn)
-                predictions = zip(*predictions_and_confidence)[0]
-
-                # Update performance metrics
-                if neg2 in predictions:
-                    correct_inds.append(ci)
-            except KeyError:
-                continue
-
-        score = 1. * len(correct_inds) / len(self.comparisons)
-        correct_comparisons = [self.comparisons[i] for i in correct_inds]
-
-        if return_correct_comparisons:
-            return score, correct_comparisons
-        else:
-            return score
+from Word2VecScorer import Word2VecScorer
 
 class get_tokenized_sentences_from_Bioscope(object):
     """Generate tokenized sentences given BioScope XML filenames.
@@ -149,6 +66,9 @@ if __name__ == "__main__":
     # Get tokenized sentences from BioScope corpus
     tokenized_sentences = get_tokenized_sentences_from_Bioscope(filenames)
 
+    #tokenized_sentences = [x for x in tokenized_sentences]
+    #tokenized_sentences = tokenized_sentences[:1000]
+
     get_sentences = lambda: tokenized_sentences;
 
     if args.verbose:
@@ -161,12 +81,20 @@ if __name__ == "__main__":
     model = gensim.models.Word2Vec(
             size=word_vector_length,
             min_count=min_word_count)
-    model.build_vocab(get_sentences())
-    model.train(get_sentences())
-    
 
-    ######################### Test model qualitatively ######################## 
-    
+    # Build vocabulary
+    model.build_vocab(get_sentences())
+
+    # Create scorer based in this model
     scorer = Word2VecScorer(model)
-    for topn in [1, 2, 3]:
-        print 'Top', topn, scorer.score(model, topn)
+
+    # Get score for each training epoch
+    n_epochs = 50
+    for ei in range(n_epochs):
+
+        # Train for one epoch
+        model.train(get_sentences())
+
+        # Evaluate model after each epoch
+        scores = [scorer.score(model, topn) for topn in [1, 2, 3, 4, 5]]
+        print ('After %i epochs, score is' % (ei + 1)), scores
---
+++ word2vec/Word2VecScorer.py
@@ -0,0 +1,88 @@
+class Word2VecScorer(object):
+    """Scorer for a word2vec or doc2vec model, using Google's set of analogous
+    comparisons.
+    """
+
+    def __init__(self, model):
+        "Takes in a word2vec or doc2vec model"
+        self.comparisons = []  # a list of 4-ples
+        self._build_comparisons(model)
+
+    def _build_comparisons(self, model,
+            evaluation_filename='data/questions-words.txt'):
+        """Build the list of comparisons appropriate for this model.
+
+        We only make use of grammar comparisons since it's the only categories
+        where our training data has the required vocabulary. We also ignore
+        comparisons if any of the four items in the comparison are not in the
+        model's vocabulary.
+        """
+
+        # Each line is a comparison 'x y X Y' i.e. x is to y what X is to Y EXCEPT
+        # for categories, which are lines that start with a colon, e.g.
+        #: city-in-state
+        #: family
+        #: gram1-adjective-to-adverb
+        #: gram2-opposite
+        with open(evaluation_filename) as fid:
+            lines = fid.read().splitlines()
+
+        # Find out where the grammar categories start.
+        start_ind = [ind for ind, line in enumerate(lines)
+                if line[0] == ':' and 'gram' in line][0]
+
+        # Grab all grammar comparisons as a list of 4-ples
+        # We happen to know all the grammar comparisons are at the end of the file.
+        vocab = set(model.vocab.keys())
+        for line in lines:
+
+            # Skip category headers
+            if line[0] == ':':
+                continue
+
+            # Don't bother testing if any of these words aren't in vocab
+            tokens = line.split()
+            if not set(tokens) <= vocab:
+                continue
+
+            self.comparisons.append(tokens)
+
+    def score(self, model, topn, return_correct_comparisons=False):
+        """Score a model by how many correct comparisons it makes from the
+        set of analogies
+
+        Score is zero if there are no comparison to be made.
+
+        topn - correct answer should be within top #N predictions
+        """
+
+        # Start comparisons
+        correct_inds = []  # Inds of comparisons that were predicted right
+        for ci, comparison in enumerate(self.comparisons):
+
+            # E.g. 'stupid clever slow fast'
+            # which implies stupid is to clever what slow is to fast.
+            # We want to predict 'fast' from the first three words
+            pos1, neg1, pos2, neg2 = comparison
+
+            try:
+
+                # Make predictions (these come with confidence levels)
+                predictions_and_confidence = model.most_similar(
+                        positive=[pos1, pos2], negative=[neg1], topn=topn)
+                predictions = zip(*predictions_and_confidence)[0]
+
+                # Update performance metrics
+                if neg2 in predictions:
+                    correct_inds.append(ci)
+            except KeyError:
+                continue
+
+        score = 1. * len(correct_inds) / len(self.comparisons)  \
+            if self.comparisons else 0
+        correct_comparisons = [self.comparisons[i] for i in correct_inds]
+
+        if return_correct_comparisons:
+            return score, correct_comparisons
+        else:
+            return score
