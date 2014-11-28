7ef7e14fccc7852078b8be0a75b7d786cd79c20d
Jia-Shen Boon <jiashen@gmail.com>
2014-11-27 22:23:18 -0600

    Evaluate word2vec model with analogies from Google
    Google provided a testset of analogies e.g cat is to milk what dog is to bone.
    With this we can quickly evaluate how well our model is performing.

--- word2vec/BioScope_word2vec.py
+++ word2vec/BioScope_word2vec.py
@@ -84,16 +84,57 @@ if __name__ == "__main__":
 
     ######################### Test model qualitatively ######################## 
 
-    test_words = ['blood', 'demonstrated']
-    for test_word in test_words:
-        if test_word not in model.vocab:
-            print 'Test word "%s" not in vocab' % test_word
+    evaluation_filename = 'data/questions-words.txt'
+    with open(evaluation_filename) as fid:
+        lines = fid.read().splitlines()
+
+    # Each line is a comparison 'x y X Y' i.e. x is to y what X is to Y EXCEPT
+    # for categories, which are lines that start with a colon, e.g.
+    #: city-in-state
+    #: family
+    #: gram1-adjective-to-adverb
+    #: gram2-opposite
+    header_inds = [ind for ind, line in enumerate(lines) if line[0] == ':']
+
+    # Find out where the grammar categories start.
+    # It's the only categories where our training data has the required
+    # vocabulary.
+    start_ind = [ind for ind, line in enumerate(lines)
+            if line[0] == ':' and 'gram' in line][0]
+
+    # Grab all grammar comparisons
+    # We happen to know all the grammar comparisons are at the end of the file.
+    comparisons = [line for line in lines[start_ind: ] if line[0] != ':']
+
+    # Start comparisons
+    n_predictions, n_correct_pred = 0, 0  # No. of predictions and correct
+                                    # predictions
+    correct_inds = []  # Inds of comparisons that were predicted right
+    vocab = set(model.vocab.keys())
+    for ci, comparison in enumerate(comparisons):
+
+        # E.g. 'stupid clever slow fast'
+        # which implies stupid is to clever what slow is to fast.
+        # We want to predict 'fast' from the first three words
+        pos1, neg1, pos2, neg2 = comparison.split()
+
+        # Don't bother testing if any of these words aren't in vocab
+        if not set(comparison.split()) <= vocab:
             continue
 
-        # Get test_words similar to this test test_word
-        similar_words = [word
-                for word, similarity in model.most_similar(test_word)]
+        try:
 
-        # Print similar words
-        print 'Words similar to "%s":' % test_word
-        print '\t', ' '.join(similar_words)
+            # Make predictions (these come with confidence levels)
+            topn = 3  # correct answer should be within top #N predictions
+            predictions_and_confidence = model.most_similar(
+                    positive=[pos1, pos2], negative=[neg1], topn=topn)
+            predictions = zip(*predictions_and_confidence)[0]
+
+            # Update performance metrics
+            if neg2 in predictions:
+                print comparison
+                n_correct_pred += 1
+                correct_inds.append(ci)
+            n_predictions += 1
+        except KeyError:
+            continue
