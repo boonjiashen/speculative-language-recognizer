82c49ca0ebaf465114078a8d1e0ee8e8ed63d58d
Jia-Shen Boon <jiashen@gmail.com>
2014-12-17 15:19:55 -0600

    Precision-recall plotter takes in 3-ples of lines

--- word2vec/plot_precision_recall.py
+++ word2vec/plot_precision_recall.py
@@ -1,12 +1,12 @@
-"""Simple script to plot precision recall curves given a file of labels and
-confidences
+"""Simple script to plot precision recall curves given a file of instance
+labels and confidences
 
-We expect the format of the input file to be the following format
-Line #1: list of labels (0s and 1s) for curve 1
-Line #2: list of confidences (higher means more confident in predicting 1) for
+The number of lines in the the input file should be in multiples of 3.
+Line #1: Name of curve 1
+Line #2: list of labels (0s and 1s) for curve 1
+Line #3: list of confidences (higher means more confident in predicting 1) for
          curve 1
-Line #3: list of labels for curve 2
-Line #4: list of confidences for curve 2
+Line #4: Name of curve 2
 etc.
 """
 
@@ -14,31 +14,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import sklearn.metrics
+import StringIO
 
-parser = argparse.ArgumentParser()
+if __name__ == "__main__":
 
-parser.add_argument('input_filename', type=str)
+    parser = argparse.ArgumentParser()
 
-args = parser.parse_args()
+    parser.add_argument('input_filename', type=str)
 
-data = np.loadtxt(args.input_filename)
+    args = parser.parse_args()
 
-plt.figure()
 
-for ci in range(0, len(data), 2):
-    labels, confidences = data[ci:ci+2]
-    precision, recall, _ = sklearn.metrics.precision_recall_curve(labels,
-            confidences)
-    label = 'Lines %i and %i' % (ci+1, ci+2)
-    plt.plot(recall, precision, label=label)
+    #################### Read lines in file ###################################
 
-plt.xlim(xmin=0)  # set axes to cut through origin
-plt.ylim(ymin=0)
-plt.xlabel('Recall')
-plt.ylabel('Precision')
+    with open(args.input_filename, 'r') as fid:
 
-# Show legend only if there's more than one curve
-if len(data) > 2:
+        # Read in lines and skip over empty lines
+        lines = [x for x in fid.read().splitlines() if x.strip()]
+
+    plt.figure()
+
+
+    #################### Plot PR curves #######################################
+
+    for ci in range(0, len(lines), 3):
+        curve_name, labels, confidences = lines[ci:ci+3]
+
+        # Remove leading and trailing spaces from curve name
+        curve_name = curve_name.strip()
+
+        # Parse strings into NumPy arrays
+        labels = [int(x) for x in labels.strip().split()]
+        confidences = np.loadtxt(StringIO.StringIO(confidences))
+
+        # Get precision, recall curve and plot it
+        precision, recall, _ = sklearn.metrics.precision_recall_curve(labels,
+                confidences)
+        plt.plot(recall, precision, label=curve_name)
+
     plt.legend(loc='best')
+    plt.xlim(xmin=0)  # set axes to cut through origin
+    plt.ylim(ymin=0)
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
 
-plt.show()
+    plt.show()
